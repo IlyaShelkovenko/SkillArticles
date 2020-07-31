14beb06fb39b548eeffba3ebbb106840e3f7de45
IlyaShelkovenko <hostov47@gmail.com>
2020-07-31 22:01:30 +0300

    init commit in branch

--- app/src/main/java/ru/skillbranch/skillarticles/ui/RootActivity.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/RootActivity.kt
@@ -9,6 +9,5 @@ class RootActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_root)
-
     }
 }
