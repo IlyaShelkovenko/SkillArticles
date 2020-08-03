741d57ed4c235f649f851254caedfbbf8c699c9d
IlyaShelkovenko <hostov47@gmail.com>
2020-08-03 22:06:02 +0300

    homework_3 with behavior done

--- app/src/main/java/ru/skillbranch/skillarticles/ui/custom/behaviors/BottombarBehavior.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/custom/behaviors/BottombarBehavior.kt
@@ -6,29 +6,42 @@ package ru.skillbranch.skillarticles.ui.custom.behaviors
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.Log
 import android.view.Gravity
 import android.view.View
 import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.math.MathUtils
 import androidx.core.view.ViewCompat
 import com.google.android.material.snackbar.Snackbar
+import ru.skillbranch.skillarticles.ui.custom.Bottombar
 import kotlin.math.max
 import kotlin.math.min
 
 
-class BottombarBehavior<V : View>(context: Context, attrs: AttributeSet) :
-    CoordinatorLayout.Behavior<V>(context, attrs) {
-
+class BottombarBehavior : CoordinatorLayout.Behavior<Bottombar>() {
     override fun onStartNestedScroll(
-        coordinatorLayout: CoordinatorLayout, child: V, directTargetChild: View, target: View, axes: Int, type: Int
+        coordinatorLayout: CoordinatorLayout,
+        child: Bottombar,
+        directTargetChild: View,
+        target: View,
+        axes: Int,
+        type: Int
     ): Boolean {
         return axes == ViewCompat.SCROLL_AXIS_VERTICAL
     }
 
     override fun onNestedPreScroll(
-        coordinatorLayout: CoordinatorLayout, child: V, target: View, dx: Int, dy: Int, consumed: IntArray, type: Int
+        coordinatorLayout: CoordinatorLayout,
+        child: Bottombar,
+        target: View,
+        dx: Int,
+        dy: Int,
+        consumed: IntArray,
+        type: Int
     ) {
+        child.translationY = MathUtils.clamp(child.translationY + dy, 0f, child.minHeight.toFloat())
         super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type)
-        child.translationY = max(0f,min(child.height.toFloat(),child.translationY + dy))
     }
+
 
 }
--- app/src/main/java/ru/skillbranch/skillarticles/ui/custom/behaviors/SubmenuBehavior.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/custom/behaviors/SubmenuBehavior.kt
@@ -4,38 +4,38 @@
 
 package ru.skillbranch.skillarticles.ui.custom.behaviors
 
-import android.content.Context
-import android.util.AttributeSet
+import android.util.Log
 import android.view.View
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.view.ViewCompat
-import kotlin.math.max
-import kotlin.math.min
+import androidx.core.view.marginRight
+import ru.skillbranch.skillarticles.ui.custom.ArticleSubmenu
+import ru.skillbranch.skillarticles.ui.custom.Bottombar
 
-class SubmenuBehavior<V : View>(context: Context, attrs: AttributeSet) :
-    CoordinatorLayout.Behavior<V>(context, attrs) {
-
-    override fun onStartNestedScroll(
-        coordinatorLayout: CoordinatorLayout,
-        child: V,
-        directTargetChild: View,
-        target: View,
-        axes: Int,
-        type: Int
+class SubmenuBehavior : CoordinatorLayout.Behavior<ArticleSubmenu>() {
+    override fun layoutDependsOn(
+        parent: CoordinatorLayout,
+        child: ArticleSubmenu,
+        dependency: View
     ): Boolean {
-        return axes == ViewCompat.SCROLL_AXIS_VERTICAL
+        return dependency is Bottombar
     }
 
-    override fun onNestedPreScroll(
-        coordinatorLayout: CoordinatorLayout,
-        child: V,
-        target: View,
-        dx: Int,
-        dy: Int,
-        consumed: IntArray,
-        type: Int
-    ) {
-        super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type)
-        child.translationY = max(0f, min(child.height.toFloat(), child.translationY + dy))
+    override fun onDependentViewChanged(
+        parent: CoordinatorLayout,
+        child: ArticleSubmenu,
+        dependency: View
+    ): Boolean {
+        return if(child.isOpen && dependency is Bottombar && dependency.translationY >= 0){
+            animate(child,dependency)
+            true
+        }else
+            false
+
     }
+}
+
+private fun animate(child: View, dependency: Bottombar){
+    val fraction = dependency.translationY/dependency.minHeight
+    child.translationX = (child.width + child.marginRight) * fraction
 }
