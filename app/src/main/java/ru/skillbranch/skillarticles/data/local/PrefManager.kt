e310a4c48ad927ad806b251fd023ef0253d17d12
IlyaShelkovenko <hostov47@gmail.com>
2020-09-09 20:03:59 +0300

    done hometask_5

---
+++ app/src/main/java/ru/skillbranch/skillarticles/data/local/PrefManager.kt
@@ -0,0 +1,23 @@
+/**
+ * Created by Ilia Shelkovenko on 02.09.2020.
+ */
+package ru.skillbranch.skillarticles.data.local
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.preference.PreferenceManager
+import ru.skillbranch.skillarticles.data.delegates.PrefDelegate
+
+class PrefManager (context : Context){
+    internal val preferences : SharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
+
+    var storedBoolean by PrefDelegate(false)
+    var storedString by PrefDelegate("test")
+    var storedInt  by PrefDelegate(Int.MAX_VALUE)
+    var storedLong  by PrefDelegate(Long.MAX_VALUE)
+    var storedFloat  by PrefDelegate(100f)
+
+    fun clearAll(){
+        preferences.edit().clear().apply()
+    }
+}
--- app/src/main/java/ru/skillbranch/skillarticles/viewmodels/ArticleViewModel.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/viewmodels/ArticleViewModel.kt
@@ -14,12 +14,14 @@ import ru.skillbranch.skillarticles.extensions.data.toAppSettings
 import ru.skillbranch.skillarticles.extensions.data.toArticlePersonalInfo
 import ru.skillbranch.skillarticles.extensions.format
 import ru.skillbranch.skillarticles.extensions.indexesOf
+import ru.skillbranch.skillarticles.markdown.MarkdownParser
 import ru.skillbranch.skillarticles.viewmodels.base.BaseViewModel
 import ru.skillbranch.skillarticles.viewmodels.base.IViewModelState
 import ru.skillbranch.skillarticles.viewmodels.base.Notify
 
 class ArticleViewModel(private val articleId: String) : BaseViewModel<ArticleState>(ArticleState()), IArticleViewModel {
     private val repository = ArticleRepository
+    private var clearContent: String? = null
 
     init {
         subscribeOnDataSource(getArticleData()){ article, state ->
@@ -30,9 +32,7 @@ class ArticleViewModel(private val articleId: String) : BaseViewModel<ArticleSta
                 category = article.category,
                 categoryIcon = article.categoryIcon,
                 date = article.date.format(),
-                author = article.author,
-                poster = article.poster,
-                content = article.content
+                author = article.author
             )
         }
 
@@ -60,7 +60,7 @@ class ArticleViewModel(private val articleId: String) : BaseViewModel<ArticleSta
         }
     }
 
-    override fun getArticleContent() : LiveData<List<Any>?>{
+    override fun getArticleContent(): LiveData<String?> {
         return repository.loadArticleContent(articleId)
     }
 
@@ -129,10 +129,12 @@ class ArticleViewModel(private val articleId: String) : BaseViewModel<ArticleSta
 
     override fun handleSearch(query: String?) {
         query ?: return
-        val result = (currentState.content.firstOrNull() as? String).indexesOf(query)
+        if(clearContent == null) clearContent = MarkdownParser.clear(currentState.content)
+        val result = clearContent
+            .indexesOf(query)
             .map {it to it + query.length}
         updateState {
-            it.copy(searchQuery = query, searchResults = result)
+            it.copy(searchQuery = query, searchResults = result,searchPosition = 0)
         }
     }
 
@@ -166,7 +168,7 @@ data class ArticleState(
     val date: String? = null,
     val author: Any? = null,
     val poster: String? = null,
-    val content: List<Any> = emptyList(),
+    val content: String? = null,
     val reviews: List<Any> = emptyList()
 ) : IViewModelState {
 
