d65c6975d2c80978e206c07220bd3d2649b07a35
IlyaShelkovenko <hostov47@gmail.com>
2020-11-07 12:20:26 +0300

    hometask_7 extension fun

---
+++ app/src/main/java/ru/skillbranch/skillarticles/extensions/BottomNavigationView.kt
@@ -0,0 +1,18 @@
+/**
+ * Created by Ilia Shelkovenko on 05.11.2020.
+ */
+package ru.skillbranch.skillarticles.extensions
+
+import androidx.navigation.NavDestination
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import ru.skillbranch.skillarticles.R
+
+fun BottomNavigationView.selectDestination(dest: NavDestination) {
+    val item = menu.findItem(dest.id)
+
+    if(item != null)
+        item.isChecked = true
+    else {
+        menu.findItem(R.id.nav_profile).isChecked = true
+    }
+}
--- app/src/main/java/ru/skillbranch/skillarticles/ui/RootActivity.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/RootActivity.kt
@@ -9,6 +9,7 @@ import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_root.*
 import kotlinx.android.synthetic.main.layout_bottombar.*
 import ru.skillbranch.skillarticles.R
+import ru.skillbranch.skillarticles.extensions.selectDestination
 import ru.skillbranch.skillarticles.ui.base.BaseActivity
 import ru.skillbranch.skillarticles.viewmodels.RootViewModel
 import ru.skillbranch.skillarticles.viewmodels.base.IViewModelState
@@ -38,7 +39,7 @@ class RootActivity : BaseActivity<RootViewModel>() {
         }
 
         navController.addOnDestinationChangedListener { controller, destination, arguments ->
-            //nav_view.selectDestination(destination)
+            nav_view.selectDestination(destination)
         }
     }
 
